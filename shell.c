ff0bdb37879e83eec23bbb94560f474ee17f2083
DevBekoDev <abubakr.ezalden.nasir@gmail.com>
2023-10-18 11:55:52 -0700

    test 0.4.1

--- shell.c
+++ shell.c
@@ -45,9 +45,5 @@ int main(int ac, char **av, char *envp[])
 	if (linesize < 0 && flags.interactive)
 		write(STDERR_FILENO, "\n", 1);
 	free(line);
-	free(command);
-	free(pathcommand);
-	free(paths);
-	free(path);
 	return (0);
 }
